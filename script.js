3aa10c290a1dc8095bf5dea10994aa3d459a21cb
Tehos <postolacheteodor@gmail.com>
2022-10-28 20:50:25 +0300

    Create function that returns random 'computerChoice' value

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let computerChoice = "";
 let playerChoice = "";
 const choices = ["Rock", "Paper", "Scissors"];
+
+const getComputerChoice = function () {
+  computerChoice = choices[Math.floor(Math.random() * 3)];
+  return computerChoice;
+};
