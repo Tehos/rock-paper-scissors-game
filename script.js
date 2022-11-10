a6e23ef4fad2400d60238771485694eba5f05c4a
Tehos <postolacheteodor@gmail.com>
2022-11-10 18:59:05 +0200

    Organize code in script.js

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 const choices = ["Rock", "Paper", "Scissors"];
 
-const getComputerChoice = function () {
-  computerChoice = choices[Math.floor(Math.random() * 3)];
-  return computerChoice;
-};
-
-// function insesitiveCase(string) {
-//   return string.charAt(0).toUpperCase() + string.substr(1).toLowerCase();
-// }
-
 let playerScore = 0;
 let computerScore = 0;
 const result = document.querySelector(".result");
 const playerScoreEl = document.querySelector(".player-score");
 const computerScoreEl = document.querySelector(".computer-score");
 
+function getComputerChoice() {
+  computerChoice = choices[Math.floor(Math.random() * 3)];
+  return computerChoice;
+}
+
 function playRound(playerSelection, computerSelection) {
-  // playerSelection = insesitiveCase(playerSelection);
   if (playerSelection == "Rock" && computerSelection == "Scissors") {
     playerScore++;
     result.textContent = `You win this round! ${playerSelection} beats ${computerSelection}.`;
@@ -35,6 +30,11 @@ function playRound(playerSelection, computerSelection) {
   return [playerScore, computerScore];
 }
 
+function countScore() {
+  playerScoreEl.textContent = `Player score: ${playerScore}`;
+  computerScoreEl.textContent = `Computer score: ${computerScore}`;
+}
+
 const btns = document.querySelectorAll("button");
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -42,38 +42,3 @@ btns.forEach((btn) => {
     countScore();
   });
 });
-
-function countScore() {
-  playerScoreEl.textContent = `Player score: ${playerScore}`;
-  computerScoreEl.textContent = `Computer score: ${computerScore}`;
-}
-
-// const playerSelection = "Rock";
-// const computerSelection = getComputerChoice();
-// console.log(playerSelection, computerSelection);
-// console.log(playRound(playerSelection, computerSelection));
-
-// function game() {
-//   let playerScore = 0;
-//   let computerScore = 0;
-//   for (i = 0; i < 5; i++) {
-//     let playerChoice = prompt("Choose Rock Paper Scissors.");
-//     let computerChoice = getComputerChoice();
-//     console.log(playerChoice, computerChoice);
-//     if (playRound(playerChoice, computerChoice) == "win") {
-//       playerScore = playerScore + 1;
-//       console.log(`You won a round! ${playerChoice} beats ${computerChoice}`);
-//     } else if (playRound(playerChoice, computerChoice) == "lose") {
-//       computerScore = computerScore + 1;
-//       console.log(`You lost a round! ${computerChoice} beats ${playerChoice}`);
-//     } else {
-//       console.log("It's a tie!");
-//       i--;
-//     }
-//   }
-//   if (playerScore > computerScore) {
-//     console.log("You won the game!");
-//   } else {
-//     console.log("You lost the game!");
-//   }
-// };
