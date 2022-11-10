2313031fb98044434dcd7486ad89e1d745cb83a6
Tehos <postolacheteodor@gmail.com>
2022-11-10 09:37:55 +0200

    Create buttons for selecting form

--- script.js
+++ script.js
@@ -24,32 +24,39 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+const btns = document.querySelectorAll("button");
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log(playRound(btn.textContent, getComputerChoice()));
+  });
+});
+
 // const playerSelection = "Rock";
 // const computerSelection = getComputerChoice();
 // console.log(playerSelection, computerSelection);
 // console.log(playRound(playerSelection, computerSelection));
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (i = 0; i < 5; i++) {
-    let playerChoice = prompt("Choose Rock Paper Scissors.");
-    let computerChoice = getComputerChoice();
-    console.log(playerChoice, computerChoice);
-    if (playRound(playerChoice, computerChoice) == "win") {
-      playerScore = playerScore + 1;
-      console.log(`You won a round! ${playerChoice} beats ${computerChoice}`);
-    } else if (playRound(playerChoice, computerChoice) == "lose") {
-      computerScore = computerScore + 1;
-      console.log(`You lost a round! ${computerChoice} beats ${playerChoice}`);
-    } else {
-      console.log("It's a tie!");
-      i--;
-    }
-  }
-  if (playerScore > computerScore) {
-    console.log("You won the game!");
-  } else {
-    console.log("You lost the game!");
-  }
-}
+// function game() {
+//   let playerScore = 0;
+//   let computerScore = 0;
+//   for (i = 0; i < 5; i++) {
+//     let playerChoice = prompt("Choose Rock Paper Scissors.");
+//     let computerChoice = getComputerChoice();
+//     console.log(playerChoice, computerChoice);
+//     if (playRound(playerChoice, computerChoice) == "win") {
+//       playerScore = playerScore + 1;
+//       console.log(`You won a round! ${playerChoice} beats ${computerChoice}`);
+//     } else if (playRound(playerChoice, computerChoice) == "lose") {
+//       computerScore = computerScore + 1;
+//       console.log(`You lost a round! ${computerChoice} beats ${playerChoice}`);
+//     } else {
+//       console.log("It's a tie!");
+//       i--;
+//     }
+//   }
+//   if (playerScore > computerScore) {
+//     console.log("You won the game!");
+//   } else {
+//     console.log("You lost the game!");
+//   }
+// };
